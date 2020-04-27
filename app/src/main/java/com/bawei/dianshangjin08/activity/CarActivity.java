49c4c5a5eb0b5f4ef21985e36789495f29f9da89
Liyize97126 <1216431704@qq.com>
2020-04-27 18:12:28 +0800

    0424-购物车复选框联动以及价格计算

--- app/src/main/java/com/bawei/dianshangjin08/activity/CarActivity.java
+++ app/src/main/java/com/bawei/dianshangjin08/activity/CarActivity.java
@@ -23,6 +23,7 @@ import com.bawei.dianshangjin08.presenter.ShoppingCartListPresenter;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 购物车页面
@@ -31,8 +32,10 @@ public class CarActivity extends BaseActivity implements IContact.IView<List<Cat
     //定义
     @BindView(R.id.shop_car_list)
     protected ExpandableListView shopCarList;
+    @BindView(R.id.check_all)
     protected CheckBox checkAll;
-    protected TextView totalMoney;
+    @BindView(R.id.total_money)
+    protected TextView totalPrice;
     private ShoppingCartListPresenter shoppingCartListPresenter;
     private ShopCarAdapter shopCarAdapter;
     private LoginInfo loginInfo;
@@ -48,6 +51,14 @@ public class CarActivity extends BaseActivity implements IContact.IView<List<Cat
         shopCarList.setAdapter(shopCarAdapter);
         shoppingCartListPresenter = new ShoppingCartListPresenter(this);
         loginInfoDao = DaoMaster.newDevSession(this, LoginInfoDao.TABLENAME).getLoginInfoDao();
+        //监听事件
+        shopCarAdapter.setOnPriceChangeListener(new ShopCarAdapter.OnPriceChangeListener() {
+            @Override
+            public void priceChange(double totalMoney, boolean isAll) {
+                checkAll.setChecked(isAll);
+                totalPrice.setText("总价 :  ￥" + totalMoney);
+            }
+        });
         //查询登录的用户
         loginInfo = loginInfoDao.queryBuilder().where(LoginInfoDao.Properties.Status.eq(1)).unique();
         //去请求
@@ -61,6 +72,12 @@ public class CarActivity extends BaseActivity implements IContact.IView<List<Cat
             shoppingCartListPresenter = null;
         }
     }
+    //全选点击事件
+    @OnClick(R.id.check_all)
+    protected void checkAll() {
+        shopCarAdapter.checkAll(checkAll.isChecked());
+    }
+    //成功回调
     @Override
     public void onViewSuccess(List<Category> result, String message) {
         //提示
@@ -69,6 +86,7 @@ public class CarActivity extends BaseActivity implements IContact.IView<List<Cat
         shopCarAdapter.getList().addAll(result);
         shopCarAdapter.notifyDataSetChanged();
     }
+    //失败回调
     @Override
     public void onViewFail(DataBean dataBean) {
         if(dataBean.getStatus().equals("1001")){
--- app/src/main/java/com/bawei/dianshangjin08/adapter/ShopCarAdapter.java
+++ app/src/main/java/com/bawei/dianshangjin08/adapter/ShopCarAdapter.java
@@ -28,6 +28,11 @@ public class ShopCarAdapter extends BaseExpandableListAdapter implements AddRedu
     public List<Category> getList() {
         return list;
     }
+    //定义回调
+    private OnPriceChangeListener onPriceChangeListener;
+    public void setOnPriceChangeListener(OnPriceChangeListener onPriceChangeListener) {
+        this.onPriceChangeListener = onPriceChangeListener;
+    }
     //一级条目总数
     @Override
     public int getGroupCount() {
@@ -82,6 +87,28 @@ public class ShopCarAdapter extends BaseExpandableListAdapter implements AddRedu
         //设置数据
         Category category = list.get(groupPosition);
         myGroupViewHouler.groupCheck.setChecked(category.isChecked());//设置商铺选中
+        //传值
+        myGroupViewHouler.groupCheck.setTag(category);
+        //点击事件
+        myGroupViewHouler.groupCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Category category = (Category) v.getTag();
+                CheckBox checkBox = (CheckBox) v;
+                //控件选中，最终的选中值必须赋值给对象进行保存
+                category.setChecked(checkBox.isChecked());
+                for (int i = 0; i < category.getShoppingCartList().size(); i++) {
+                    //取出商品
+                    ShoppingCartList shoppingCartList = category.getShoppingCartList().get(i);
+                    //商品的选中状态需要看商铺是否选中
+                    shoppingCartList.setChecked(category.isChecked());
+                }
+                //计算总价，价格回调
+                totalMoney();
+                //刷新适配器
+                notifyDataSetChanged();
+            }
+        });
         myGroupViewHouler.groupCheck.setText(category.getCategoryName());
         return convertView;
     }
@@ -105,6 +132,40 @@ public class ShopCarAdapter extends BaseExpandableListAdapter implements AddRedu
         //设置数据
         ShoppingCartList shoppingCartList = list.get(groupPosition).getShoppingCartList().get(childPosition);
         myChildViewHouler.childCheck.setChecked(shoppingCartList.isChecked());//设置商品选中
+        //寄存商品对象
+        myChildViewHouler.childCheck.setTag(shoppingCartList);
+        //联动：点击监听
+        myChildViewHouler.childCheck.setOnClickListener(new View.OnClickListener() {//匿名内部类
+            @Override
+            public void onClick(View v) {
+                //得到当前集合对象
+                ShoppingCartList cartList = (ShoppingCartList) v.getTag();
+                //得到当前控件
+                CheckBox checkBox = (CheckBox) v;
+                //设置控件选中状态
+                cartList.setChecked(checkBox.isChecked());
+                //和商铺选项联动（商品如果都选中，商铺必须选中，商品有一个未选中，商铺则不能被选中。）
+                for (int i = 0; i < list.size(); i++) {
+                    //取出商铺
+                    Category category = list.get(i);
+                    //取出商铺下所有商品
+                    List<ShoppingCartList> cartLists = list.get(i).getShoppingCartList();
+                    //定义一个标志值
+                    boolean categoryCheck = true;//可用if判断的逻辑来处理，通过使用&&，如果有一个未选中则最终结果就是false
+                    for (int j = 0; j < cartLists.size(); j++) {
+                        //获取商品对象
+                        ShoppingCartList sCartList = cartLists.get(j);
+                        categoryCheck = categoryCheck && sCartList.isChecked();//有一个sCartList商品的状态未选中，最终结果就是false
+                    }
+                    //修改商铺选中状态
+                    category.setChecked(categoryCheck);
+                }
+                //计算总价，价格回调
+                totalMoney();
+                //刷新适配器
+                notifyDataSetChanged();
+            }
+        });
         myChildViewHouler.childName.setText(shoppingCartList.getCommodityName());
         myChildViewHouler.childPrice.setText("￥" + shoppingCartList.getPrice());
         //设置加减器（设置数量，设置商品对象，设置监听器）
@@ -133,6 +194,49 @@ public class ShopCarAdapter extends BaseExpandableListAdapter implements AddRedu
         int goodsCount = view.getGoodsCount();
         ShoppingCartList shoppingCartList = (ShoppingCartList) view.getTag();
         shoppingCartList.setCount(goodsCount);
+        //计算总价，价格回调
+        totalMoney();
+    }
+    //全选方法
+    public void checkAll(boolean isAll){
+        for (int i = 0; i < list.size(); i++) {
+            //设置商铺全选
+            list.get(i).setChecked(isAll);
+            List<ShoppingCartList> shoppingCartList = list.get(i).getShoppingCartList();
+            for (int j = 0; j < shoppingCartList.size(); j++) {
+                //设置商品全选
+                shoppingCartList.get(j).setChecked(isAll);
+            }
+        }
+        //计算总价，价格回调
+        totalMoney();
+        //刷新适配器
+        notifyDataSetChanged();
+    }
+    //计算价格，而且最终决定是否全选和反选
+    public void totalMoney() {
+        //定义
+        boolean isAll = true;
+        double sumPrice = 0;
+        for (int i = 0; i < list.size(); i++) {
+            //取出商铺对象
+            Category category = list.get(i);
+            //商铺如果全选，那就是全选
+            isAll = category.isChecked() && isAll;
+            //取出商铺下所有商品
+            List<ShoppingCartList> shoppingCartList = category.getShoppingCartList();
+            for (int j = 0; j < shoppingCartList.size(); j++) {
+                //获得商品对象
+                ShoppingCartList cartList = shoppingCartList.get(j);
+                if(cartList.isChecked()){
+                    sumPrice += (cartList.getPrice() * cartList.getCount());//价格*数量
+                }
+            }
+        }
+        //回调页面
+        if(onPriceChangeListener != null){
+            onPriceChangeListener.priceChange(sumPrice,isAll);
+        }
     }
     //寄存器
     class MyGroupViewHouler{
@@ -143,5 +247,9 @@ public class ShopCarAdapter extends BaseExpandableListAdapter implements AddRedu
         ImageView childImage;
         TextView childName,childPrice;
         AddReduceView childAddReduce;
+    }
+    //声明反馈接口
+    public interface OnPriceChangeListener{
+        void priceChange(double totalMoney,boolean isAll);
     }
 }
